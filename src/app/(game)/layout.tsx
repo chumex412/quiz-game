25896702dc5a7352d114db51e41c4c917354448a
Kode Navigator <chumex412@gmail.com>
2025-06-13 18:59:33 +0100

    fix: fixes client error

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -1,12 +1,23 @@
 import AppSidebar from "@/components/common/AppSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactNode } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">{children}</main>
-    </SidebarProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">{children}</main>
+      </SidebarProvider>
+    </QueryClientProvider>
   );
 }
